903dc50cbb25866817f4dffebc443121fbc13335
kimchan122 <chanchankim122@gmail.com>
2022-02-17 03:33:20 +0900

    달력이 이제 변경됩니다!

--- src/views/pages/Reservation/Reservation.js
+++ src/views/pages/Reservation/Reservation.js
@@ -1,20 +1,20 @@
-//import React from "react";
-import Header from "components/Headers/Header.js";
-import React, {useState, useEffect} from "react";
+import React, {useCallback, useState, useEffect} from "react";
 import { Card, CardBody, CardTitle, Container, Row, Col, CardHeader, CardGroup } from "reactstrap";
-//import Header from "components/Headers/Header.js";
+import Header from "components/Headers/Header.js";
 import style from "views/pages/DashBoard/style";
 import {Calendar, momentLocalizer} from 'react-big-calendar'
-//import { Calendar as BigCalendar, momentLocalizer } from "react-big-calendar";
 import moment from 'moment'
-//import Firebase from 'firebase';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+//import Firebase from 'firebase';
 
 const newEvents = [];
+const dateObj=new Date();
 
 const Reservation = () => {
 
   const [eventDb, setEventDb] = useState([]);
+
+  const [day, setDay] = useState({dateObj});
 
 
   useEffect(() => {
@@ -32,14 +32,20 @@ const Reservation = () => {
   },[]);
 
 console.log(newEvents);
-
-//},[] );
   
   let today = new Date();
+
   let formats ={
     timeGutterFormat: (date, culture, localizer) => 
           localizer.format(date, 'H:mm', culture),
   }
+
+  const onSelectDate = (date) => {
+    const newDate= new Date(date);
+    setDay(newDate);
+    console.log("day: ",day)
+  }
+
   /*const onSelectEvent = function(pEvent) {
     const r = window.confirm("Weet je zeker dat je de afspraak wilt verwijderen?'")
     if(r === true){
@@ -55,7 +61,13 @@ console.log(newEvents);
 
     }
   }*/
-
+  const ColoredDateCellWrapper = ({ children }) =>
+  React.cloneElement(React.Children.only(children), {
+    style: {
+      //backgroundColor: 'lightblue', // 캘린더 내부 색상
+    },
+  })
+  
   moment.locale('ko-KR');
   const localizer = momentLocalizer(moment)
   const myEventsList = [
@@ -74,10 +86,15 @@ console.log(newEvents);
   return (
     <>
       <Header />
+
         <Container fluid>
+
           <Card className="card-stats mb-4 mb-xl-0 col-md-30 col-sm-60 h-100">
+
             <Row>
+
               <Col xl="8">
+
                 <Calendar
                   events={eventDb}
                   localizer={localizer}
@@ -94,6 +111,8 @@ console.log(newEvents);
                   resourceIdAccessor="resourceId"
                   resourceTitleAccessor="resourceTitle"
                   culture={moment.locale('ko-KR')}
+                  //onSelectSlot={handleSelect}
+                  date={day}
                   messages={{
                     today: "오늘",
                     previous: "<",
@@ -102,95 +121,146 @@ console.log(newEvents);
                     week: "Week",
                     day: "Dag"
                   }}
-              />
-              </Col>
-              <Col>
-              <Row>
-                <Col lg="10" xl="12">
-                  {/* 타석 종류 */}
-                <Card className="card-stats mb-4 mb-xl-10 col-md-30 col-sm-60">
-                  <CardBody className="bg-primary">
-                  <div className="col">
-                      <CardTitle
-                      tag="h1"
-                      className="text-uppercase mb-0"
-                    >
-                      타석 종류
-                    </CardTitle>
-                      </div>
-                  </CardBody>
-                  <CardBody>
-                    <Row>
-                      <div>
-                        <p>타석 종류 표시란</p>
-                      </div>
-                    </Row>
-                  </CardBody>
-                </Card>
-                </Col>
-
-                <Col>
-                <Calendar
-                  events={eventDb}
-                  localizer={localizer}
-                  defaultView={'month'}
-                  views ={['month']}
-                  timeslots={4}
-                  step={15}
-                  defaultDate={new Date()}
-                  formats={formats}
-                  //onSelectEvent = {event => onSelectEvent(event)}
-                  min={new Date(today.getFullYear(),today.getMonth(), today.getDate(), 0)}
-                  max={new Date(today.getFullYear(), today.getMonth(), today.getDate(), 23)}
-                  resources={resourceMap}
-                  resourceIdAccessor="resourceId"
-                  resourceTitleAccessor="resourceTitle"
-                  culture={moment.locale('ko-KR')}
-                  messages={{
-                    //today: "오늘",
-                    previous: "<",
-                    next: ">",
+                  components={{
+                    timeSlotWrapper: ColoredDateCellWrapper,
                   }}
-                  style={{ height: 300 }}
               />
+
+              </Col>
+
+              <Col>
+
+                <Row>
+                  
+                  <Col lg="10" xl="12">
+                    
+                    {/* 타석 종류 */}
+                    <Card className="card-stats mb-4 mb-xl-10 col-md-30 col-sm-60">
+                    
+                      <CardBody className="bg-primary">
+                    
+                        <div className="col">
+                      
+                          <CardTitle
+                            tag="h1"
+                            className="text-uppercase mb-0"
+                          >
+                            타석 종류
+                          </CardTitle>
+                      
+                        </div>
+                      
+                      </CardBody>
+  
+                      <CardBody>
+  
+                        <Row>
+  
+                          <div>
+  
+                            <p>타석 종류 표시란</p>
+  
+                          </div>
+  
+                        </Row>
+  
+                      </CardBody>
+  
+                    </Card>
+  
+                  </Col>
+  
+                  <Col>
+
+                    <Calendar
+                      //events={eventDb}
+                      localizer={localizer}
+                      defaultView={'month'}
+                      views ={['month']}
+                      timeslots={4}
+                      step={15}
+                      defaultDate={new Date()}
+                      formats={formats}
+                      //onSelectEvent = {event => onSelectEvent(event)}
+                      min={new Date(today.getFullYear(),today.getMonth(), today.getDate(), 0)}
+                      max={new Date(today.getFullYear(), today.getMonth(), today.getDate(), 23)}
+                      resources={resourceMap}
+                      resourceIdAccessor="resourceId"
+                      resourceTitleAccessor="resourceTitle"
+                      culture={moment.locale('ko-KR')}
+                      messages={{
+                        today: "오늘",
+                        previous: "<",
+                        next: ">",
+                      }}
+                      style={{ height: 300 }}
+                      //selectable={true}
+                      onNavigate={(date)=>{
+                        console.log('#### date=',date)
+                        onSelectDate(date)
+                      }}
+                    />
               
-                </Col>
+                  </Col>
                 
-                <Col lg="10" xl="12">
+                  <Col lg="10" xl="12">
+                  
                   {/* 예약 상세 내용 */}
-                  <Card className="card-stats mb-4 mb-xl-10 col-md-30 col-sm-60">
-                  <CardBody className="bg-primary">
-                  <div className="col">
-                      <CardTitle
-                      tag="h1"
-                      className="text-uppercase mb-0"
-                    >
-                      예약 상세 내용
-                    </CardTitle>
-                      </div>
-                  </CardBody>
-                  <CardBody>
-                    <Row>
-                      <div>
-                        <p>예약 상세 내용란</p>
-                      </div>
-                    </Row>
-                  </CardBody>
-                </Card>
-                </Col>
-              </Row>
+                    <Card className="card-stats mb-4 mb-xl-10 col-md-30 col-sm-60">
+                      
+                      <CardBody className="bg-primary">
+                      
+                        <div className="col">
+                      
+                          <CardTitle
+                            tag="h1"
+                            className="text-uppercase mb-0"
+                          >
+                          
+                          예약 상세 내용
+                          </CardTitle>
+                      
+                        </div>
+                      
+                      </CardBody>
+                      
+                      <CardBody>
+                      
+                        <Row>
+                      
+                          <div>
+                      
+                            <p>예약 상세 내용란</p>
+                      
+                          </div>
+                      
+                        </Row>
+                      
+                      </CardBody>
+                  
+                    </Card>
+
+                  </Col>
+                
+                </Row>
+              
               </Col>
 
             </Row>
+            
             <CardBody>
 
             </CardBody>
+            
             <CardBody
               style={{height: 300}}>
               asdf
             </CardBody>
+
           </Card>
+
         </Container>
+        
     </>
   );
 };
