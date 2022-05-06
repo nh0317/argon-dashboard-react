e84591663b701279bc6c32db1fa8cbde74343073
DESKTOP-G3G8GKG\psy <sooy8951@gmail.com>
2022-05-06 16:59:36 +0900

    fix: 로그인 오류 수정

--- src/views/pages/Signin.js
+++ src/views/pages/Signin.js
@@ -28,59 +28,56 @@ import {
 
     const [err0, setErr0] = useState("");
 
+
     const onLogin = (e) => {
-        e.preventDefault();
-        console.log(e);
-        
-        const data = {
-            "id" : id,
-            "password" : pw
-        };
+      e.preventDefault();
+      console.log(e);
+      
+      const data = {
+          "id" : id,
+          "password" : pw
+      };
 
-        try{
-
-        axios.post('/partner/login', data).then(
-        onLoginSuccess)
-        .catch(error => {
-            setErr0("로그인에 실패했습니다.");
-            console.log(error);
-        })
-    }catch(e){
-        console.log(e);
-    }}
-
-    const onSilentRefresh = () => {
-const response =         axios.post('/users/refresh');
-console.log(response);
-            onLoginSuccess(response);
-    }
-    
-    const onLoginSuccess = response => {
-     
-        console.log(response);
-
-                const  accessToken  = response.data.result.jwt;
-                const jwtValidity = response.data.result.jwtValidity;
-
-                
-                // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
-                axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;    
-                setTimeout(onSilentRefresh, jwtValidity * 1000 - 60000);
-  // accessToken 만료하기 1분 전에 로그인 연장
+      try{
+        console.log(data)
+      axios.post('/partner/login', data).then(res=>{
+          if(onLoginSuccess(res))
+          history.push("/admin/dashboard");
+      })
+      .catch(error => {
+          setErr0("로그인에 실패했습니다.");
+          console.log(error);
+      })
+  }catch(e){
+      console.log(e);
+  }}
+  const onSilentRefresh = async () => {
+      let response = await axios.post('/users/refresh');
+      console.log("refresh now");
+      onLoginSuccess(response);
+  }
   
-                const code = response.data.code;
-                if(code==2000 || code==3000 || code==3010){
-                    setErr0("로그인에 실패했습니다.");
-                }
-                else if (code==2030){
-                    setErr0("비밀번호가 틀렸습니다. 비밀번호를 다시 입력해주세요.");
-                }
-                else{
-                    history.push("/admin/dashboard");
-                }      
+  const onLoginSuccess = response => {
+    console.log(response)
+              const  accessToken  = response.data.result.jwt;
+              const jwtValidity = response.data.result.jwtValidity;
 
+              // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
+              axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;    
+              setTimeout(onSilentRefresh, jwtValidity * 1000 - 60000);
+               // accessToken 만료하기 1분 전에 로그인 연장
 
+              const code = response.data.code;
+              if(code==2000 || code==3000 || code==3010){
+                  setErr0("로그인에 실패했습니다.");
+              }
+              else if (code==2030){
+                  setErr0("비밀번호가 틀렸습니다. 비밀번호를 다시 입력해주세요.");
+              }
+              else
+                  return true;
       }
+
     return (
       <>
         <Col lg="5" md="7">
